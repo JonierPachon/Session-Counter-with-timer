98e298437ada19e29e12e5d62a30d4d6764acb2f
Jonier Pachon <jhonier.pachon1015@gmail.com>
2025-09-06 15:56:10 -0500

    Implement new functionality.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const containerInputs = document.querySelector(".inputs");
+
 const hoursInput = document.querySelector("#hours");
 const minutesInput = document.querySelector("#minutes");
 const secondsInput = document.querySelector("#seconds");
@@ -5,7 +7,12 @@ const secondsInput = document.querySelector("#seconds");
 const startBtn = document.querySelector("#start");
 const display = document.querySelector("#display");
 
+const resetBtn = document.querySelector("#reset-btn");
+const cancelBtn = document.querySelector("#cancel-btn");
+const pauseBtn = document.querySelector("#pause-btn");
+
 let timerId;
+let pauseTime = true;
 
 function pad(value) {
    return String(value).padStart(2, "0");
@@ -23,11 +30,7 @@ const inputs = [hoursInput, minutesInput, secondsInput];
 const maxValues = [12, 59, 59];
 
 inputs.forEach((input, index) => {
-   input.addEventListener("input", () => {
-      input.value = input.value.replace(/\D/g, "").slice(0, 2);
-   });
-
-   // input.addEventListener("blur", () => formatInput(input, maxValues[index]));
+   input.addEventListener("input", () => formatInput(input, maxValues[index]));
    input.addEventListener("change", () => formatInput(input, maxValues[index]));
 });
 
@@ -42,11 +45,23 @@ function updateDisplay(endTime) {
 
    if (totalSeconds <= 0) {
       clearInterval(timerId);
+      toggleTimer(false);
    }
 }
 
-startBtn.addEventListener("click", () => {
-   inputs.forEach((input, index) => formatInput(input, maxValues[index]));
+function toggleTimer(show) {
+   startBtn.classList.toggle("close", show);
+
+   containerInputs.classList.toggle("close", show);
+   display.classList.toggle("close", !show);
+   resetBtn.classList.toggle("close", !show);
+   cancelBtn.classList.toggle("close", !show);
+   pauseBtn.classList.toggle("close", !show);
+}
+
+function startTimer() {
+   toggleTimer(true);
+
    const hours = parseInt(hoursInput.value, 10) || 0;
    const minutes = parseInt(minutesInput.value, 10) || 0;
    const seconds = parseInt(secondsInput.value, 10) || 0;
@@ -60,4 +75,25 @@ startBtn.addEventListener("click", () => {
 
    clearInterval(timerId);
    timerId = setInterval(() => updateDisplay(endTime), 1000);
+}
+startBtn.addEventListener("click", () => {
+   startTimer();
+});
+
+cancelBtn.addEventListener("click", () => {
+   clearInterval(timerId);
+   toggleTimer(false);
+});
+
+resetBtn.addEventListener("click", () => {
+   startTimer();
+});
+
+pauseBtn.addEventListener("click", () => {
+   if (pauseTime) {
+      clearInterval(timerId);
+      pauseTime = false;
+   } else {
+      startTimer();
+   }
 });
